2754d1163324f5fefcaec7456543485253d65c72
Kapil1813 <Varadarajankapil@gmail.com>
2026-03-18 03:55:15 -0700

    Add files via upload

--- app2.py
+++ app2.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # -----------------------------
 # Optional OpenAI Setup (safe)
 # -----------------------------
