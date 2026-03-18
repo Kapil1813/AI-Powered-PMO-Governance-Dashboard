50080eadfb94ee4ecb369f999e596a231cb46c51
Kapil1813 <Varadarajankapil@gmail.com>
2026-03-18 04:01:10 -0700

    Add files via upload

--- app2.py
+++ app2.py
@@ -7,15 +7,14 @@ import os
 # -----------------------------
 # Optional OpenAI Setup (safe)
 # -----------------------------
-USE_AI = False
-client = None
-try:
-    from openai import OpenAI
-    api_key = os.getenv("OPENAI_API_KEY")
-    if api_key:
-        client = OpenAI(api_key=api_key)
-        USE_AI = True
-except Exception:
+import os
+from openai import OpenAI
+
+api_key = os.getenv("OPENAI_API_KEY")
+if api_key:
+    client = OpenAI(api_key=api_key)
+    USE_AI = True
+else:
     USE_AI = False
 
 # -----------------------------
